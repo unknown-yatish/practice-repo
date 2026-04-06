1089fef8786a4a88f19f7ee55d09633701455a90
unknown-yatish <24aiml063@bnmit.in>
2026-04-06 12:32:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("hello world ")
+from flask import Flask
+app =Flask(__name__)
+@app.route('/')
+def home():
+  return ('index.html',title='home')
+if __name__=='__main__':
+  app.run(host='127.0.0.1',port=8080,debug=True)
