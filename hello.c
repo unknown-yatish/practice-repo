0498b596f1a06ee19842359cbb2659188b0c11bb
unknown-yatish <24aiml063@bnmit.in>
2026-04-08 17:10:54 +0530

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,4 +1,16 @@
-#include<stdio.h>
-void main(){
-    printf("hello world");
-}
+git config --global user.name"unknown-yatish"
+git config --global user.email"24aiml063@bnmit.in"
+git config --global core.editor "code --wait"
+    git config --global -e
+
+mkdir 
+cd 
+git init
+echo hello>file.txt
+echo hello >file1.txt
+git log 
+git add file.txt
+git commit -m "1st"
+git add .
+git commit -m "2nd"
+rest in the new repo 
